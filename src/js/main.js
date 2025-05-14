0b55902ce79ef7ba73fdb1a4a5cf69402e036516
Daniel Cikora <d.cikora98@gmail.com>
2025-05-14 22:51:31 +0200

    Navigation, Hero, Sidebar Sections partially done.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
-const hamburgerButton = document.querySelector(".hamburger-btn");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navigation = document.querySelector(".navigation");
+const hamburgerButton = document.querySelector(".navigation__hamburger-button");
+const hamburgerMenu = document.querySelector(".navigation__content");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    navigation.classList.add("navigation--scrolled");
+  } else {
+    navigation.classList.remove("navigation--scrolled");
+  }
+});
 
 if (hamburgerButton && hamburgerMenu) {
   hamburgerButton.addEventListener("click", () => {
-    hamburgerMenu.classList.toggle("hamburger-menu--opened");
+    hamburgerMenu.classList.toggle("navigation__content--active");
   });
 }

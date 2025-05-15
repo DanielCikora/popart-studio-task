ce3e7b06fcfcb0e54d137a79eaa6172bfd15cbf9
Daniel Cikora <d.cikora98@gmail.com>
2025-05-15 09:53:17 +0200

    Navigation - Done, Responsive Done.

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
-const navigation = document.querySelector(".navigation");
+const header = document.querySelector(".header");
 const hamburgerButton = document.querySelector(".navigation__hamburger-button");
 const hamburgerMenu = document.querySelector(".navigation__content");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
-    navigation.classList.add("navigation--scrolled");
+    header.classList.add("header--scrolled");
   } else {
-    navigation.classList.remove("navigation--scrolled");
+    header.classList.remove("header--scrolled");
   }
 });
 
 if (hamburgerButton && hamburgerMenu) {
   hamburgerButton.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("navigation__content--active");
+    document.body.classList.toggle("no-scroll");
   });
 }

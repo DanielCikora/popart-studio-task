2d8214c1eb6bbff97bcc1598fa49bc1794fad77b
Daniel Cikora <d.cikora98@gmail.com>
2025-05-15 19:00:26 +0200

    WhyUs - Section Done, Responsive Done.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const header = document.querySelector(".header");
 const hamburgerButton = document.querySelector(".navigation__hamburger-button");
 const hamburgerMenu = document.querySelector(".navigation__content");
+const whyUsBoxes = document.querySelectorAll(".whyus__box");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
@@ -16,3 +17,27 @@ if (hamburgerButton && hamburgerMenu) {
     document.body.classList.toggle("no-scroll");
   });
 }
+
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const box = entry.target;
+      const delay = box.dataset.index * 400;
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          box.classList.add("whyus__box--visible");
+        }, delay);
+      } else {
+        box.classList.remove("whyus__box--visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+whyUsBoxes.forEach((box, i) => {
+  box.dataset.index = i;
+  intersectionObserver.observe(box);
+});
